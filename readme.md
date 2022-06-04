b42bb10910362a4228dcaab0e947e24788b93a3a
QuanTA <65522530+quantrananh2304@users.noreply.github.com>
2022-06-04 19:09:08 +0700

    init

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Tailwind + Webpack boilerplate
+## Node16 recommended
+## How to run
+```
+npm install
+```
+
+### Development:
+```
+npm run tailwind
+```
+Open another terminal
+```
+npm run start
+```
+
+### Production
+```
+npm run build
+```
+Use the recommended way installing `http-server` with `npm install -g http-server` and inside the `dist` folder run `http-server -p 9090` to finally open the url `http://localhost:9090` to see the production.
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // Note: order is last -> first
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 9000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
