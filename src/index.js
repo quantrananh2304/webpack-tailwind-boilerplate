089b86d9d113b0d44cc9a454392868dde44a852d
QuanTA <65522530+quantrananh2304@users.noreply.github.com>
2022-06-06 00:16:16 +0700

    third component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+import image1 from "./assets/images/image-1.svg";
+import image2 from "./assets/images/image-2.svg";
+import image3 from "./assets/images/image-3.svg";
+import image4 from "./assets/images/image-4.svg";
+import icon1 from "./assets/icons/icon-1.svg";
+import icon2 from "./assets/icons/icon-2.svg";
+import headerBackground from "./assets/images/header-background.svg";
+
+document.getElementById("image-2").setAttribute("src", image2);
+document.getElementById("image-3").setAttribute("src", image3);
+document.getElementById("image-4").setAttribute("src", image4);
+document.getElementById("icon-1").setAttribute("src", icon1);
+document.getElementById("icon-2").setAttribute("src", icon2);
