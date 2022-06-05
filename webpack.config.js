0d56df84875099a2565f45713cd66d1653e03c8a
QuanTA <65522530+quantrananh2304@users.noreply.github.com>
2022-06-05 22:48:26 +0700

    webpack config images

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,14 @@ module.exports = {
           "postcss-loader",
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   devServer: {
