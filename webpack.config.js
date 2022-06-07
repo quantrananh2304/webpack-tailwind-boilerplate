9ca6f9a2915f190278529222210b17a5610e28ed
HoanDM <65522530+quantrananh2304@users.noreply.github.com>
2022-06-07 15:45:51 +0700

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name][ext]",
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[name][ext]",
+        },
       },
     ],
   },
